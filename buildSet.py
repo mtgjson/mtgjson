903e742e74758d29c515f6b1ce986cd1eeee4d22
Zach Halpern <Zahalpern+github@gmail.com>
2018-05-21 23:44:20 -0400

    more fields added

--- buildSet.py
+++ buildSet.py
@@ -53,7 +53,7 @@ class GetChecklistURLs:
         :return: Dictionary (Set, URLs)
         """
         sets_to_download = gathererSets.get_gatherer_sets()
-        main_url = "http://gatherer.wizards.com/Pages/Search/Default.aspx?{}"
+        main_url = 'http://gatherer.wizards.com/Pages/Search/Default.aspx?{}'
 
         return_value = {}
 
@@ -72,7 +72,7 @@ class GetChecklistURLs:
         return return_value
 
 
-class DownloadCardsBySet:
+class GenerateMIDsBySet:
     # Class Variable
     all_set_multiverse_ids = []
     set_name = ""
@@ -94,20 +94,147 @@ class DownloadCardsBySet:
                 self.all_set_multiverse_ids.append(card_m_id)
 
     def get_multiverse_ids_from_set(self):
-        return {self.set_name: self.all_set_multiverse_ids}
+        return self.all_set_multiverse_ids
 
     def clear(self):
         self.set_name = ""
         self.all_set_multiverse_ids = []
 
 
-if __name__ == "__main__":
+class DownloadsCardsByMIDList:
+    # Class Variables
+    set_name = ""
+    multiverse_ids = []
+    cards_in_set = {}
+
+    def __init__(self, set_name, multi_ids):
+        self.set_name = set_name
+        self.multiverse_ids = multi_ids
+        self.create_cards()
+
+    def create_cards(self):
+        main_url = 'http://gatherer.wizards.com/Pages/Card/Details.aspx?{}'
+        div_name = 'ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_{}'
+
+        for card_m_id in multiverse_ids:
+            url_for_info = main_url.format(self.get_url_params(card_m_id))
+
+            with urllib.request.urlopen(url_for_info) as response:
+                html = response.read()
+                card_info = {}
+
+                """ Get Card Multiverse ID """
+                card_info['multiverseid'] = card_m_id
+
+                soup = BeautifulSoup(html.decode(), 'html.parser')
+
+                """ Get Card Name """
+                name_row = soup.find(id=div_name.format('nameRow'))
+                name_row = name_row.findAll('div')[-1]
+                card_name = str(name_row.contents)[6:].lstrip()[:-2]
+
+                card_info['name'] = card_name
+                print(card_name)
+
+                """ Get Card CMC, Colors, and Color Identity (start) """
+                mana_row = soup.find(id=div_name.format('manaRow'))
+                mana_row = mana_row.findAll('div')[-1]
+                mana_row = mana_row.findAll('img')
+
+                card_cmc = 0
+                card_color_identity = []
+                card_colors = []
+
+                for symbol in mana_row:
+                    symbol_value = symbol['alt']
+                    if symbol_value.isdigit():
+                        card_cmc += int(symbol_value)
+                    elif symbol_value != 'X':
+                        card_cmc += 1
+                        card_color_identity.append(symbol_value)
+                        card_colors.append(symbol_value[0])
+
+                # Remove duplicates
+                card_colors = list(set(card_colors))
+
+                card_info['cmc'] = card_cmc
+                card_info['colors'] = card_colors
+                print(card_cmc, card_color_identity, card_colors)
+
+                """ Get Card Type(s) """
+                card_super_types = []
+                card_types = []
+                card_sub_types = []
+                type_row = soup.find(id=div_name.format('typeRow'))
+                type_row = type_row.findAll('div')[-1]
+                type_row = str(type_row.contents)[6:].lstrip()[:-2]
+
+                if '—' in type_row:
+                    type_split = type_row.split('—')
+
+                    for value in type_split[0].split(' '):
+                        if value in gathererSets.get_super_types():
+                            card_super_types.append(value)
+                        elif value in gathererSets.get_types():
+                            card_types.append(value)
+
+                    for value in type_split[1].split(' '):
+                        card_sub_types.append(value)
+                else:
+                    for value in type_row.split(' '):
+                        if value in gathererSets.get_super_types():
+                            card_super_types.append(value)
+                        elif value in gathererSets.get_types():
+                            card_types.append(value)
+
+                # Remove empty values from the lists
+                card_super_types = list(filter(None, card_super_types))
+                card_types = list(filter(None, card_types))
+                card_sub_types = list(filter(None, card_sub_types))
+
+                card_info['supertypes'] = card_super_types
+                card_info['types'] = card_types
+                card_info['subtypes'] = card_sub_types
+                print(type_row, card_super_types, card_types, card_sub_types)
+
+                text_row = soup.find(id=div_name.format('textRow'))
+                text_row = text_row.findAll('div')[-1]
+                text_row = str(text_row)[52:-6] # Cannot use .contents as it messes with images
+
+                text_soup = BeautifulSoup(text_row, 'html.parser')
+                for symbol in text_soup.findAll('img'):
+                    symbol_value = symbol['alt']
+                    symbol.replace_with("{{{0}}}".format(symbol_value))
+
+                card_info['text'] = str(text_soup)
+                print(text_soup)
+
+                # Insert new value
+                self.cards_in_set[card_m_id] = card_info
+                print("--"*10)
+
+    @staticmethod
+    def get_url_params(card_m_id):
+        url_params = urllib.parse.urlencode({
+            'multiverseid': '{0}'.format(card_m_id),
+            'printed': 'false'
+        })
+        return url_params
+
+    def get_cards_in_set(self):
+        return self.cards_in_set
+
+
+if __name__ == '__main__':
     urls = GetChecklistURLs().get_key_with_urls()
 
-    download_holder = DownloadCardsBySet()
+    m_ids_holder = GenerateMIDsBySet()
     for key, value in urls.items():
-        download_holder.parse_urls(key, value)
+        #m_ids_holder.parse_urls(key, value)
+        #multiverse_ids = m_ids_holder.get_multiverse_ids_from_set()
+        #m_ids_holder.clear()
 
-        multiverse_ids = download_holder.get_multiverse_ids_from_set()
-        download_holder.clear()
-        print(multiverse_ids)
+        multiverse_ids = ['417574', '417575', '417576', '417577', '417578', '417579']
+
+        cards_holder = DownloadsCardsByMIDList(key, multiverse_ids).get_cards_in_set()
+        print(cards_holder)
--- gathererSets.py
+++ gathererSets.py
@@ -1,10 +1,10 @@
 def get_gatherer_sets():
     gatherer_sets = [
-        'Kaladesh',
-        'Aether Revolt'
+        'Kaladesh'
+
     ]
     """
-        
+        'Aether Revolt'
         'Amonkhet',
         'Welcome Deck 2017',
         'Hour of Devastation',
@@ -13,3 +13,44 @@ def get_gatherer_sets():
         'Dominaria'
     """
     return gatherer_sets
+
+
+def get_super_types():
+    super_types = [
+        'Basic',
+        'Legendary',
+        'Snow',
+        'World',
+        'Ongoing'
+    ]
+    return super_types
+
+
+def get_types():
+    types = [
+        'Instant',
+        'Sorcery',
+        'Artifact',
+        'Creature',
+        'Enchantment',
+        'Land',
+        'Planeswalker',
+        'Tribal',
+        'Plane',
+        'Phenomenon',
+        'Scheme',
+        'Vanguard',
+        'Conspiracy',
+        'Host',
+
+        # Un-Sets
+        'Enchant',
+        'Player',
+        'Interrupt',
+        'Scariest',
+        'You\'ll',
+        'Ever',
+        'See',
+        'Eaturecray'
+    ]
+    return types
