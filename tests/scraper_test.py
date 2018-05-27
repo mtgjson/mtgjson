a6f83996160bc5a2fc522589f48ad261c95a84ab
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-05-28 02:17:08 +1000

    Speaking of Unit Tests...

---
+++ tests/scraper_test.py
@@ -0,0 +1,16 @@
+import asyncio
+import aiohttp
+import vcr
+from mtgjson4 import builder
+import pytest
+
+@pytest.mark.asyncio
+@vcr.use_cassette
+async def test_w17(event_loop: asyncio.AbstractEventLoop) -> None:
+    W17 = builder.determine_gatherer_sets({'sets': ['W17'], 'all_sets': False})
+    b = builder.MtgJson(W17, loop=event_loop)
+    json = await b.build_set(W17[0], 'en')
+    assert json["block"] == 'Amonkhet'
+    assert json["border"] == 'black'
+    assert len(json['cards']) == 30
+    assert json['code'] == 'W17'
