d9f13ec3f33da53e9bf0ea8827653a813adf6c2a
Zach Halpern <Zahalpern+github@gmail.com>
2018-05-27 23:13:31 -0400

    Alphabetical and location

--- tests/scraper_test.py
+++ tests/scraper_test.py
@@ -1,8 +1,7 @@
 import asyncio
+from mtgjson4 import mtg_builder
 import pytest
 import vcr
-
-from mtgjson4 import mtg_builder
 
 
 @pytest.mark.asyncio
@@ -12,7 +11,7 @@ async def test_w17(event_loop: asyncio.AbstractEventLoop) -> None:
     Very basic test.  Can the scraper scrape Gatherer?
     """
     w17 = mtg_builder.determine_gatherer_sets({'sets': ['W17'], 'all_sets': False})
-    b = mtg_builder.MtgJson(w17, loop=event_loop)
+    b = mtg_builder.MTGJSON(w17, loop=event_loop)
     json = await b.build_set(w17[0], 'en')
     assert json["block"] == 'Amonkhet'
     assert json["border"] == 'black'
@@ -27,7 +26,7 @@ async def test_isd(event_loop: asyncio.AbstractEventLoop) -> None:
     Scrape Innistrad.  Make sure those DFCs work.
     """
     isd = mtg_builder.determine_gatherer_sets({'sets': ['ISD'], 'all_sets': False})
-    b = mtg_builder.MtgJson(isd, loop=event_loop)
+    b = mtg_builder.MTGJSON(isd, loop=event_loop)
     json = await b.build_set(isd[0], 'en')
     delver = [c for c in json['cards'] if c['multiverseid'] == 226749][0]
     assert delver['name'] == 'Delver of Secrets'
