5b1b676f4ca85a443d9e27bc880b70bdcfe642df
tooomm <tooomm@users.noreply.github.com>
2018-05-31 16:41:46 +0200

    update language map

--- mtgjson4/mtg_global.py
+++ mtgjson4/mtg_global.py
@@ -757,15 +757,17 @@ SYMBOL_MAP: Dict[str, str] = {
 }
 
 LANGUAGE_MAP: Dict[str, str] = {
-    'cn': 'Chinese Traditional',
     'de': 'German',
     'en': 'English',
     'es': 'Spanish',
     'fr': 'French',
     'it': 'Italian',
     'jp': 'Japanese',
+    'ko': 'Korean',
     'pt': 'Portuguese (Brazil)',
-    'ru': 'Russian'
+    'ru': 'Russian',
+    'zh-Hans': 'Chinese Simplified',
+    'zh-Hant': 'Chinese Traditional'
 }
 
 # These file names cannot be used on WindowsOS
