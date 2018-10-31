32b08ac5f9fa74466b9a788a2c1d421ddf5b9bdf
Zach Halpern <ZaHalpern+github@gmail.com>
2018-10-30 20:59:44 -0400

    reformatting

--- mtgjson4/compile_mtg.py
+++ mtgjson4/compile_mtg.py
@@ -34,10 +34,12 @@ def build_output_file(sf_cards: List[Dict[str, Any]], set_code: str) -> Dict[str
     output_file["type"] = set_config.get("set_type")
 
     # Add booster info based on boosters resource (manually maintained for the time being)
-    with pathlib.Path(mtgjson4.RESOURCE_PATH, "boosters.json").open("r", encoding="utf-8") as f:
+    with pathlib.Path(mtgjson4.RESOURCE_PATH, "boosters.json").open(
+        "r", encoding="utf-8"
+    ) as f:
         json_dict: Dict[str, List[Any]] = json.load(f)
         if output_file["code"].upper() in json_dict.keys():
-            output_file['booster'] = json_dict[output_file["code"].upper()]
+            output_file["booster"] = json_dict[output_file["code"].upper()]
 
     if set_config.get("block"):
         output_file["block"] = set_config.get("block")
