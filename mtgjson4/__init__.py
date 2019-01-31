519bcdf4226d221199573aae8562f2655c10764c
Zach Halpern <ZaHalpern+github@gmail.com>
2019-01-30 21:58:53 -0500

    Version 4.2.2 number for minor fixes

--- mtgjson4/__init__.py
+++ mtgjson4/__init__.py
@@ -5,8 +5,8 @@ import time
 from typing import Dict, List
 
 # Maintenance variables
-__VERSION__ = "4.2.1"
-__VERSION_DATE__ = "2018-01-12"
+__VERSION__ = "4.2.2"
+__VERSION_DATE__ = "2018-01-30"
 __MAINTAINER__ = "Zach Halpern (GitHub: @ZeldaZach)"
 __MAINTAINER_EMAIL__ = "zahalpern+github@gmail.com"
 __REPO_URL__ = "https://github.com/mtgjson/mtgjson4"
