78146d2dc6578f556b4e621ea9d24a28f446a110
Zach H <zahalpern+github@gmail.com>
2019-03-19 19:27:37 -0400

    Add translations (#284)
    Signed-off-by: Zach Halpern <ZaHalpern+github@gmail.com>

--- mtgjson4/__init__.py
+++ mtgjson4/__init__.py
@@ -22,6 +22,7 @@ RESOURCE_PATH: pathlib.Path = TOP_LEVEL_DIR.joinpath("mtgjson4").joinpath("resou
 SESSION_CACHE_EXPIRE_GATHERER: int = 604800  # seconds - 1 week
 SESSION_CACHE_EXPIRE_TCG: int = 604800  # seconds - 1 week
 SESSION_CACHE_EXPIRE_SCRYFALL: int = 604800  # seconds - 1 week
+SESSION_CACHE_EXPIRE_MKM: int = 604800  # seconds - 1 week
 
 
 # Compiled Output Files
