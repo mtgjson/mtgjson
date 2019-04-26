0a9e3213635ea529374d66f5758b23e52ba0fbc1
Zach H <zahalpern+github@gmail.com>
2019-04-26 04:15:06 -0400

    Compression & Cleanup (#327)
    * Add compression operation to the build script (-z)
    Cleanup for global variables
    MP uses cpu count over hardcoded value
    Outputter fixed up so no longer has to have specific ordering
    
    Signed-off-by: Zach Halpern <ZaHalpern+github@gmail.com>
    
    * Readme + Cleanup
    
    Signed-off-by: Zach Halpern <ZaHalpern+github@gmail.com>

--- mtgjson4/__init__.py
+++ mtgjson4/__init__.py
@@ -13,23 +13,26 @@ __MAINTAINER__ = "Zach Halpern (GitHub: @ZeldaZach)"
 __MAINTAINER_EMAIL__ = "zahalpern+github@gmail.com"
 __REPO_URL__ = "https://github.com/mtgjson/mtgjson"
 
-# Globals
+# Globals -- constant types
 SUPERTYPES: List[str] = ["Basic", "Host", "Legendary", "Ongoing", "Snow", "World"]
 BASIC_LANDS: List[str] = ["Plains", "Island", "Swamp", "Mountain", "Forest"]
+
+# Globals -- paths
 TOP_LEVEL_DIR: pathlib.Path = pathlib.Path(__file__).resolve().parent.parent
 COMPILED_OUTPUT_DIR: pathlib.Path = TOP_LEVEL_DIR.joinpath("json_" + __VERSION__)
 LOG_DIR: pathlib.Path = TOP_LEVEL_DIR.joinpath("logs")
 CONFIG_PATH: pathlib.Path = TOP_LEVEL_DIR.joinpath("mtgjson.properties")
 RESOURCE_PATH: pathlib.Path = TOP_LEVEL_DIR.joinpath("mtgjson4").joinpath("resources")
+
+# Globals -- caches
 SESSION_CACHE_EXPIRE_GENERAL: int = 604800  # seconds - 1 week
 SESSION_CACHE_EXPIRE_TCG: int = 604800  # seconds - 1 week
 SESSION_CACHE_EXPIRE_SCRYFALL: int = 604800  # seconds - 1 week
 SESSION_CACHE_EXPIRE_MKM: int = 604800  # seconds - 1 week
 SESSION_CACHE_EXPIRE_STOCKS: int = 604800  # seconds - 1 week
-
 USE_CACHE: contextvars.ContextVar = contextvars.ContextVar("USE_CACHE")
 
-# MKM Globals
+# Globals -- MKM applications
 os.environ["MKM_APP_TOKEN"] = ""
 os.environ["MKM_APP_SECRET"] = ""
 os.environ["MKM_ACCESS_TOKEN"] = ""
@@ -39,20 +42,37 @@ MTGSTOCKS_BUFFER: str = "20202"
 CARD_MARKET_BUFFER: str = "10101"
 
 # Compiled Output Files
-ALL_SETS_OUTPUT: str = "AllSets"
 ALL_CARDS_OUTPUT: str = "AllCards"
-SET_LIST_OUTPUT: str = "SetList"
-COMPILED_LIST_OUTPUT: str = "CompiledList"
-KEY_WORDS_OUTPUT: str = "Keywords"
-VERSION_OUTPUT: str = "version"
-STANDARD_OUTPUT: str = "Standard"
-MODERN_OUTPUT: str = "Modern"
-VINTAGE_OUTPUT: str = "Vintage"
-REFERRAL_DB_OUTPUT: str = "ReferralMap"
-ALL_SETS_DIR_OUTPUT: str = "AllSetFiles"
 ALL_DECKS_DIR_OUTPUT: str = "AllDeckFiles"
+ALL_SETS_DIR_OUTPUT: str = "AllSetFiles"
+ALL_SETS_OUTPUT: str = "AllSets"
 CARD_TYPES_OUTPUT: str = "CardTypes"
+COMPILED_LIST_OUTPUT: str = "CompiledList"
 DECK_LISTS_OUTPUT: str = "DeckLists"
+KEY_WORDS_OUTPUT: str = "Keywords"
+MODERN_OUTPUT: str = "Modern"
+REFERRAL_DB_OUTPUT: str = "ReferralMap"
+SET_LIST_OUTPUT: str = "SetList"
+STANDARD_OUTPUT: str = "Standard"
+VERSION_OUTPUT: str = "version"
+VINTAGE_OUTPUT: str = "Vintage"
+
+OUTPUT_FILES: List[str] = [
+    ALL_CARDS_OUTPUT,
+    ALL_DECKS_DIR_OUTPUT,
+    ALL_SETS_DIR_OUTPUT,
+    ALL_SETS_OUTPUT,
+    CARD_TYPES_OUTPUT,
+    COMPILED_LIST_OUTPUT,
+    DECK_LISTS_OUTPUT,
+    KEY_WORDS_OUTPUT,
+    MODERN_OUTPUT,
+    REFERRAL_DB_OUTPUT,
+    SET_LIST_OUTPUT,
+    STANDARD_OUTPUT,
+    VERSION_OUTPUT,
+    VINTAGE_OUTPUT,
+]
 
 # Provider tags
 SCRYFALL_PROVIDER_ID = "sf"
---
+++ mtgjson4/compressor.py
@@ -0,0 +1,111 @@
+""" MTGJSON Compression Tools """
+import bz2
+import gzip
+import logging
+import lzma
+import multiprocessing
+import pathlib
+import shutil
+from typing import Any, Callable, List
+import zipfile
+
+import mtgjson4
+
+LOGGER = logging.getLogger(__name__)
+
+
+def compress_output_folder() -> None:
+    """
+    Compress all files within the output folder, to prepare for
+    uploads to production
+    """
+    sql_files = [file for file in mtgjson4.COMPILED_OUTPUT_DIR.glob("*.sqlite")]
+
+    set_files = [
+        file
+        for file in mtgjson4.COMPILED_OUTPUT_DIR.glob("*.json")
+        if file.stem not in mtgjson4.OUTPUT_FILES
+    ]
+
+    compiled_files = [
+        file
+        for file in mtgjson4.COMPILED_OUTPUT_DIR.glob("*.json")
+        if file.stem in mtgjson4.OUTPUT_FILES
+    ]
+
+    deck_files = [
+        file for file in mtgjson4.COMPILED_OUTPUT_DIR.joinpath("decks").glob("*.json")
+    ]
+
+    # Compress individual files
+    LOGGER.info("Compressing individual files")
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        pool.map(compress_file, sql_files + compiled_files + set_files + deck_files)
+
+    # Compress folders for each set file, and each deck file
+    compress_directory(set_files, "AllSetFiles")
+    compress_directory(deck_files, "AllDeckFiles")
+
+
+def compress_directory(files_to_compress: List[pathlib.Path], output_name: str) -> None:
+    """
+    Compress a directory using all supported compression methods
+    :param files_to_compress: Files to compress into a single archive
+    :param output_name: Name to give compressed archive
+    """
+    temp_dir = mtgjson4.COMPILED_OUTPUT_DIR.joinpath(output_name)
+
+    # Copy files to temporary folder
+    LOGGER.info("Creating temporary directory for {}".format(output_name))
+    temp_dir.mkdir(parents=True, exist_ok=True)
+    for file in files_to_compress:
+        shutil.copy(str(file), str(temp_dir))
+
+    # Compress the archives
+    LOGGER.info("Compressing {}".format(temp_dir.name))
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        # Compression methods
+        pool.apply_async(shutil.make_archive, (temp_dir, "bztar", str(temp_dir)))
+        pool.apply_async(shutil.make_archive, (temp_dir, "gztar", str(temp_dir)))
+        pool.apply_async(shutil.make_archive, (temp_dir, "xztar", str(temp_dir)))
+        pool.apply_async(shutil.make_archive, (temp_dir, "zip", str(temp_dir)))
+
+        # Wait for compressions to finish
+        pool.close()
+        pool.join()
+
+    # Delete the temporary folder
+    LOGGER.info("Removing temporary directory for {}".format(output_name))
+    shutil.rmtree(temp_dir, ignore_errors=True)
+
+
+def compress_file(file_path: pathlib.Path) -> None:
+    """
+    Compress a single file using all supported compression methods
+    :param file_path: Path of file to compress
+    """
+    LOGGER.info("Compressing {}".format(file_path.name))
+    __generic_compressor(file_path, ".bz2", bz2.compress)
+    __generic_compressor(file_path, ".gz", gzip.compress)
+    __generic_compressor(file_path, ".xz", lzma.compress)
+
+    # Zip files are done a bit differently
+    with zipfile.ZipFile(str(file_path) + ".zip", "w") as zip_out:
+        zip_out.write(file_path, file_path.name, zipfile.ZIP_DEFLATED)
+
+
+def __generic_compressor(
+    input_file_path: pathlib.Path,
+    file_ending: str,
+    compress_function: Callable[[Any], Any],
+) -> None:
+    """
+    Compress a single file
+    :param input_file_path: File to compress
+    :param file_ending: Ending to add onto archive
+    :param compress_function: Function that compresses
+    """
+    output_file_path = pathlib.Path(str(input_file_path) + file_ending)
+
+    with input_file_path.open("rb") as f_in, output_file_path.open("wb") as f_out:
+        f_out.write(compress_function(f_in.read()))
