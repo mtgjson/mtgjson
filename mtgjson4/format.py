7d85747df712fcc097c5ea15b489e5d6c921f7b7
Henry Jewell <henry.wj.jewell@gmail.com>
2019-09-12 00:20:25 +0100

    rename to build_format_map and make return value format-centric

--- mtgjson4/format.py
+++ mtgjson4/format.py
@@ -1,6 +1,9 @@
 import json
 import logging
 
+logger = logging.getLogger(__name__)
+
+
 FORMATS = (
     'standard',
     'future',
@@ -16,14 +19,11 @@ FORMATS = (
 )
 
 
-logger = logging.getLogger(__name__)
-
-
 class UnrecognizedFormatException(Exception):
     pass  # TODO: Define MTGJSON exception hierarchy.
 
 
-def find_formats_for_sets(all_sets_path):
+def build_format_map(all_sets_path):
     """
     For each set in the specified JSON file, determine its legal sets and return a dictionary mapping set code to
     a list of legal formats.
@@ -37,7 +37,7 @@ def find_formats_for_sets(all_sets_path):
     try:
         with open(all_sets_path, encoding='utf-8', mode='r') as all_sets_json:
             all_sets = json.load(all_sets_json)
-            set_formats = {}
+            formats = {fmt: [] for fmt in FORMATS}
 
             for code, data in all_sets.items():
                 possible_formats = set(FORMATS)
@@ -47,9 +47,10 @@ def find_formats_for_sets(all_sets_path):
                     card_formats = set(card.get('legalities').keys())
                     possible_formats &= card_formats
 
-                set_formats[code] = possible_formats
+                for fmt in possible_formats:
+                    formats[fmt].append(code)
 
-            return set_formats
+            return formats
 
     except IOError:
         logger.exception("Could not open {}.".format(all_sets_path))
@@ -58,23 +59,3 @@ def find_formats_for_sets(all_sets_path):
     except json.JSONDecodeError:
         logger.exception("Could not decode JSON from {}.".format(all_sets_path))
         raise  # TODO: Handle this properly.
-
-
-def build_format_subset(target_format, format_map):
-    """
-    Given a format and a format mapping, produce a subset of sets that are legal in this format.
-
-    :param target_format: The format to find sets for
-    :type target_format: str
-    :param format_map: Mapping between set codes and formats in which they are legal
-    :type format_map: dict
-
-    :return: List of set codes that are legal in the specified format
-    :rtype: [str]
-    """
-    return list(
-        filter(
-            lambda code: target_format in format_map[code],
-            format_map.keys()
-        )
-    )
