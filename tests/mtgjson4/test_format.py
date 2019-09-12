9312d664a84833ad5f6fded81a0ddd5d3627ca53
Henry Jewell <henry.wj.jewell@gmail.com>
2019-09-12 22:33:20 +0100

    add unit test for build_format_map

---
+++ tests/mtgjson4/test_format.py
@@ -0,0 +1,68 @@
+import pytest
+
+from mtgjson4.format import build_format_map
+
+
+NULL_OUTPUT = {
+    'standard': [],
+    'future': [],
+    'modern': [],
+    'legacy': [],
+    'vintage': [],
+    'commander': [],
+    'brawl': [],
+    'pauper': [],
+    'penny': [],
+    'oldschool': [],
+    'duel': []
+}
+
+
+@pytest.mark.parametrize(
+    "all_sets, expected",
+    [
+        pytest.param({}, NULL_OUTPUT),
+        pytest.param(
+            {
+                'TS1': {
+                    'cards': [
+                        {'legalities': {'standard': 'Legal'}}
+                    ]
+                },
+                'TS2': {
+                    'cards': [
+                        {'legalities': {'standard': 'Legal'}},
+                        {'legalities': {'standard': 'Legal', 'modern': 'Legal'}}
+                    ]
+                },
+                'TS3': {
+                    'cards': [
+                        {'legalities': {'modern': 'Legal', 'standard': 'Legal'}},
+                        {'legalities': {'modern': 'Legal'}}
+                    ]
+                },
+                'TS4': {
+                    'cards': [
+                        {'legalities': {'vintage': 'Restricted', 'legacy': 'Legal', 'modern': 'Legal', 'standard': 'Legal'}},
+                        {'legalities': {'vintage': 'Legal', 'legacy': 'Legal', 'modern': 'Legal', 'standard': 'Banned'}}
+                    ]
+                }
+            },
+            {
+                **NULL_OUTPUT,
+                **{
+                    'standard': ['TS1', 'TS2', 'TS4'],
+                    'modern': ['TS3', 'TS4'],
+                    'legacy': ['TS4'],
+                    'vintage': ['TS4']
+                }
+            }
+        )
+    ]
+)
+def test_build_format_map(all_sets: dict, expected: dict) -> None:
+    """
+    Tests that set legality is determined correctly, given a range of inputs
+    """
+    result = build_format_map(all_sets, regular=False)
+    assert(result == expected)
