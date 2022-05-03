6a2110db4d394a0ab464a611f609bbfbf5ba1ea7
Zach H <zahalpern+github@gmail.com>
2022-05-03 03:26:48 +0100

    Add testing around missing cards (#941)
    Fix #931

---
+++ tests/mtgjson5/test_all_cards_downloaded.py
@@ -0,0 +1,27 @@
+import mtgjson5.providers
+import pytest
+import mtgjson5.set_builder
+
+
+@pytest.mark.parametrize(
+    "card_name_to_find, card_number_to_find, set_code",
+    [
+        ["Briarbridge Tracker", "172", "MID"],
+        ["Candlelit Cavalry", "175", "MID"],
+    ],
+)
+def test(card_name_to_find, card_number_to_find, set_code):
+    """
+    These cards have gone missing in the past due to upstream caching issues
+    """
+    scryfall_data = mtgjson5.providers.scryfall.ScryfallProvider().download_cards(
+        set_code
+    )
+
+    scryfall_data_map_name = {card["name"]: card for card in scryfall_data}
+    scryfall_data_map_number = {
+        card["collector_number"]: card for card in scryfall_data
+    }
+
+    assert card_name_to_find in scryfall_data_map_name.keys()
+    assert card_number_to_find in scryfall_data_map_number.keys()
