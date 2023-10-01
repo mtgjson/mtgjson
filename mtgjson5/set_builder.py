d75cb7603826ba27b15d1307877c59afa2cb99f4
Sam Zimmerman <samzimmerman@me.com>
2023-09-30 18:22:53 -0700

    Update sealed product data sources.

--- mtgjson5/set_builder.py
+++ mtgjson5/set_builder.py
@@ -488,18 +488,17 @@ def build_mtgjson_set(set_code: str) -> Optional[MtgjsonSetObject]:
     mtgjson_set.booster = GitHubBoostersProvider().get_set_booster_data(set_code)
 
     # Build sealed product using the TCGPlayer data
-    mtgjson_set.sealed_product = (
-        TCGPlayerProvider().generate_mtgjson_sealed_product_objects(
-            mtgjson_set.tcgplayer_group_id, mtgjson_set.code
-        )
-    )
-    CardKingdomProvider().update_sealed_product(
-        mtgjson_set.name, mtgjson_set.sealed_product
-    )
+    #mtgjson_set.sealed_product = (
+    #    TCGPlayerProvider().generate_mtgjson_sealed_product_objects(
+    #        mtgjson_set.tcgplayer_group_id, mtgjson_set.code
+    #    )
+    #)
+    #CardKingdomProvider().update_sealed_product(
+    #    mtgjson_set.name, mtgjson_set.sealed_product
+    #)
     sealed_provider = GitHubSealedProvider()
-    mtgjson_set.sealed_product.extend(
-        sealed_provider.get_sealed_products_data(set_code)
-    )
+    mtgjson_set.sealed_product = sealed_provider.get_sealed_products_data(set_code)
+    
     sealed_provider.apply_sealed_contents_data(set_code, mtgjson_set)
     add_sealed_uuid(mtgjson_set)
     add_sealed_purchase_url(mtgjson_set)
