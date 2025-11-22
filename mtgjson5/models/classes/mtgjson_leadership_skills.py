3afd8ae1fd77e7808ccbef9d94243c362d7aa171
Robert Pratt <rpratt1337@gmail.com>
2025-11-22 05:33:33 -0600

    Added improved docstring and field descriptions for clarity on commander eligibility formats in MtgjsonLeadershipSkillsObject

--- mtgjson5/models/classes/mtgjson_leadership_skills.py
+++ mtgjson5/models/classes/mtgjson_leadership_skills.py
@@ -1,13 +1,24 @@
 """MTGJSON Leadership Skills Object model for commander eligibility."""
 
+from pydantic import Field
+
 from ..mtgjson_base import MTGJsonModel
 
 
 class MtgjsonLeadershipSkillsObject(MTGJsonModel):
     """
-    MTGJSON Singular Card.LeadershipSkills Object
+    The Leadership Skills Data Model describes the properties of formats that a card is legal to be your Commander.
     """
 
-    brawl: bool
-    commander: bool
-    oathbreaker: bool
+    brawl: bool = Field(
+        default=False,
+        description="If the card can be your commander in the Standard Brawl format.",
+    )
+    commander: bool = Field(
+        default=False,
+        description="If the card can be your commander in the Commander/EDH format.",
+    )
+    oathbreaker: bool = Field(
+        default=False,
+        description="If the card can be your commander in the Oathbreaker format.",
+    )
