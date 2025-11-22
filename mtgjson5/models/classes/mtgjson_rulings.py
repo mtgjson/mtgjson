7e90a4f9a7ee11fd7226155b371539159a10ffcb
Robert Pratt <rpratt1337@gmail.com>
2025-11-22 05:37:48 -0600

    improved docstring and add field descriptions in MtgjsonRulingObject

--- mtgjson5/models/classes/mtgjson_rulings.py
+++ mtgjson5/models/classes/mtgjson_rulings.py
@@ -1,12 +1,14 @@
 """MTGJSON Rulings Object model for official card rulings."""
 
+from pydantic import Field
+
 from ..mtgjson_base import MTGJsonModel
 
 
 class MtgjsonRulingObject(MTGJsonModel):
     """
-    MTGJSON Singular Card.Rulings Object
+    The Rulings Data Model describes the properties of rulings for a card.
     """
 
-    date: str
-    text: str
+    date: str = Field(alias="published_at", description="The release date in ISO 8601 format for the rule.")
+    text: str = Field(alias="comment", description="The text ruling of the card.")
