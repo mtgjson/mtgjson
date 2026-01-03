b30540dfecf332f9f0f74d1b300627bcab523f5f
Robert Pratt <rpratt1337@gmail.com>
2026-01-03 17:49:54 -0600

    feat(models/scryfall): add Scryfall models subpackage
    New subpackage for typed Scryfall API models.
    Exports:
    - Literals: BorderColor, Color, Layout, Rarity, etc.
    - Models: ScryfallCard, ScryfallSet, ScryfallList, etc.

---
+++ mtgjson5/mtgjson_models/scryfall/__init__.py
@@ -0,0 +1,45 @@
+from .literals import (
+     BorderColor,
+     Color,
+     Component,
+     Finish,
+     Frame,
+     FrameEffect,
+     Game,
+     ImageStatus,
+     Layout,
+     LegalityStatus,
+     ManaColor,
+     Rarity,
+     SecurityStamp,
+)
+from .submodels import (
+     ScryfallBulkData,
+     ScryfallCard,
+     ScryfallList,
+     ScryfallRuling,
+     ScryfallSet,
+)
+
+
+__all__ = [
+     "BorderColor",
+     "Color",
+     "Component",
+     "Finish",
+     "Frame",
+     "FrameEffect",
+     "Game",
+     "ImageStatus",
+     "Layout",
+     "LegalityStatus",
+     "ManaColor",
+     "Rarity",
+     "ScryfallBulkData",
+     "ScryfallCard",
+     "ScryfallList",
+     "ScryfallRuling",
+     "ScryfallSet",
+     "SecurityStamp",
+]
+
