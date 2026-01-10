21b8de15ed853e68971c629ddfbe9765e300212d
Robert Pratt <rpratt1337@gmail.com>
2026-01-10 07:04:46 -0600

    refactor(consts): reorganize imports and constants for clarity

--- mtgjson5/consts/__init__.py
+++ mtgjson5/consts/__init__.py
@@ -11,88 +11,88 @@ Usage:
 
 from __future__ import annotations
 
-# Layout constants
-from mtgjson5.consts.layouts import (
-	LayoutVariant,
-	MULTIFACE_LAYOUTS,
-	FACE_NAME_LAYOUTS,
-	FACE_MANA_VALUE_LAYOUTS,
-	TOKEN_LAYOUTS,
-	SPLIT_WATERMARK_LAYOUTS,
-	SPLIT_COLOR_LAYOUTS,
-	is_multiface,
-	is_token_layout,
-)
-
 # Expansion/Set constants
 from mtgjson5.consts.expansions import (
-	Expansion,
-	SetType,
 	DUEL_DECK_SETS,
 	FOIL_NONFOIL_LINK_SETS,
 	FUNNY_SETS_WITH_ACORN,
+	Expansion,
+	SetType,
 )
 
 # Field constants
 from mtgjson5.consts.fields import (
-	SORTED_LIST_FIELDS,
+	ALLOW_IF_FALSEY,
+	EXCLUDE_FROM_OUTPUT,
+	IDENTIFIERS_FIELD_SOURCES,
+	OMIT_EMPTY_LIST_FIELDS,
+	OMIT_FIELDS,
+	OPTIONAL_BOOL_FIELDS,
+	OTHER_OPTIONAL_FIELDS,
 	REQUIRED_CARD_LIST_FIELDS,
 	REQUIRED_DECK_LIST_FIELDS,
 	REQUIRED_LIST_FIELDS,
-	OMIT_EMPTY_LIST_FIELDS,
-	OPTIONAL_BOOL_FIELDS,
 	REQUIRED_SET_BOOL_FIELDS,
-	OTHER_OPTIONAL_FIELDS,
-	EXCLUDE_FROM_OUTPUT,
-	OMIT_FIELDS,
-	ALLOW_IF_FALSEY,
-	IDENTIFIERS_FIELD_SOURCES,
+	SORTED_LIST_FIELDS,
 )
+
+# Finish constants
+from mtgjson5.consts.finishes import FINISH_ORDER
 
 # Language constants
 from mtgjson5.consts.languages import LANGUAGE_MAP
 
-# Finish constants
-from mtgjson5.consts.finishes import FINISH_ORDER
+# Layout constants
+from mtgjson5.consts.layouts import (
+	FACE_MANA_VALUE_LAYOUTS,
+	FACE_NAME_LAYOUTS,
+	MULTIFACE_LAYOUTS,
+	SPLIT_COLOR_LAYOUTS,
+	SPLIT_WATERMARK_LAYOUTS,
+	TOKEN_LAYOUTS,
+	LayoutVariant,
+	is_multiface,
+	is_token_layout,
+)
 
 # Mapping constants
 from mtgjson5.consts.mappings import TYPEDDICT_FIELD_ALIASES
 
 
 __all__ = [
-	# Layouts
-	"LayoutVariant",
-	"MULTIFACE_LAYOUTS",
-	"FACE_NAME_LAYOUTS",
-	"FACE_MANA_VALUE_LAYOUTS",
-	"TOKEN_LAYOUTS",
-	"SPLIT_WATERMARK_LAYOUTS",
-	"SPLIT_COLOR_LAYOUTS",
-	"is_multiface",
-	"is_token_layout",
-	# Expansions
-	"Expansion",
-	"SetType",
+	"ALLOW_IF_FALSEY",
 	"DUEL_DECK_SETS",
+	"EXCLUDE_FROM_OUTPUT",
+	"FACE_MANA_VALUE_LAYOUTS",
+	"FACE_NAME_LAYOUTS",
+	# Finishes
+	"FINISH_ORDER",
 	"FOIL_NONFOIL_LINK_SETS",
 	"FUNNY_SETS_WITH_ACORN",
-	# Fields
-	"SORTED_LIST_FIELDS",
-	"REQUIRED_CARD_LIST_FIELDS",
-	"REQUIRED_DECK_LIST_FIELDS",
-	"REQUIRED_LIST_FIELDS",
-	"OMIT_EMPTY_LIST_FIELDS",
-	"OPTIONAL_BOOL_FIELDS",
-	"REQUIRED_SET_BOOL_FIELDS",
-	"OTHER_OPTIONAL_FIELDS",
-	"EXCLUDE_FROM_OUTPUT",
-	"OMIT_FIELDS",
-	"ALLOW_IF_FALSEY",
 	"IDENTIFIERS_FIELD_SOURCES",
 	# Languages
 	"LANGUAGE_MAP",
-	# Finishes
-	"FINISH_ORDER",
+	"MULTIFACE_LAYOUTS",
+	"OMIT_EMPTY_LIST_FIELDS",
+	"OMIT_FIELDS",
+	"OPTIONAL_BOOL_FIELDS",
+	"OTHER_OPTIONAL_FIELDS",
+	"REQUIRED_CARD_LIST_FIELDS",
+	"REQUIRED_DECK_LIST_FIELDS",
+	"REQUIRED_LIST_FIELDS",
+	"REQUIRED_SET_BOOL_FIELDS",
+	# Fields
+	"SORTED_LIST_FIELDS",
+	"SPLIT_COLOR_LAYOUTS",
+	"SPLIT_WATERMARK_LAYOUTS",
+	"TOKEN_LAYOUTS",
 	# Mappings
 	"TYPEDDICT_FIELD_ALIASES",
+	# Expansions
+	"Expansion",
+	# Layouts
+	"LayoutVariant",
+	"SetType",
+	"is_multiface",
+	"is_token_layout",
 ]
