4b1b7e984b1a374cd5cbb03c6279f948569f50de
Zach H <zahalpern+github@gmail.com>
2026-02-12 19:46:01 -0500

    tox

--- mtgjson5/v2/models/__init__.py
+++ mtgjson5/v2/models/__init__.py
@@ -266,7 +266,7 @@ def write_typescript_interfaces(path: str, **kwargs: Any) -> None:
     import re
 
     content = generate_typescript_interfaces(**kwargs)
-    
+
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, "w") as f:
         f.write(content)
